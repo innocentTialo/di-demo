b092192244289a221832890eb5e097b71bf6517a
InnocentTIALO <tialoinnocent@gmail.com>
2020-03-21 04:50:08 +0100

    Initial commit - small project doing to understanding how spring dependency injection works

---
+++ src/main/java/com/ddf/mainteam/DiDemoApplication.java
@@ -0,0 +1,20 @@
+package com.ddf.mainteam;
+
+import com.ddf.mainteam.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+
+        controller.hello();
+    }
+
+}
---
+++ src/main/java/com/ddf/mainteam/controllers/MyController.java
@@ -0,0 +1,14 @@
+package com.ddf.mainteam.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by InnocentTIALO on 3/15/2020.
+ */
+@Controller
+public class MyController {
+
+    public void hello () {
+        System.out.println("Hello");
+    }
+}
